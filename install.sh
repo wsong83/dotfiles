9eae575e5efa52eed179c229e351f77761b1cc4f
Wei Song (宋威) <wsong83@gmail.com>
2019-05-31 16:31:48 +0800

    add automake

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt-get install aptitude
 sudo aptitude -y install emacs emacs-goodies-extra-el vim meld source-highlight openssh-server
 sudo aptitude -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
 sudo aptitude -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev
+sudo aptitude -y autotools-dev automake
 sudo aptitude -y install csh lsb-core device-tree-compiler ncurses-dev
 sudo aptitude -y install unrar wine chrpath repo
 
