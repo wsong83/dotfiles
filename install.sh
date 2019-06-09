ec02b12e12c5fce2d6fdb24d01336f06fa4b3c74
Wei Song (宋威) <wsong83@gmail.com>
2019-06-09 15:09:08 +0800

    add more fonts

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get install aptitude
 
 sudo aptitude -y install emacs emacs-goodies-extra-el vim meld source-highlight openssh-server
 sudo aptitude -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
-sudo aptitude -y install texlive-science texlive-latex-extra
+sudo aptitude -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
 sudo aptitude -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev
 sudo aptitude -y autotools-dev automake libreadline-dev libelf-dev swig
 sudo aptitude -y install csh lsb-core device-tree-compiler ncurses-dev
