b02b52f80c2f60f3ccf024911a09a320ce502aae
Wei Song (宋威) <wsong83@gmail.com>
2019-06-29 11:32:54 +0800

    add ifconfig

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd ~
 
 sudo apt-get install aptitude
 
-sudo aptitude -y install emacs emacs-goodies-extra-el vim meld source-highlight openssh-server
+sudo aptitude -y install emacs emacs-goodies-extra-el vim meld source-highlight openssh-server net-tools
 sudo aptitude -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
 sudo aptitude -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
 sudo aptitude -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev
