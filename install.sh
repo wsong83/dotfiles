74bdca52d5b3b8519c36e8a01ab36b86f18407ea
Wei Song (宋威) <wsong83@gmail.com>
2019-07-13 13:33:08 +0800

    fix typo and add python-pip

--- install.sh
+++ install.sh
@@ -1,20 +1,18 @@
 #!/usr/bash
 
-cd ~
-
 sudo apt-get install aptitude
 
 sudo aptitude -y install emacs emacs-goodies-extra-el vim meld source-highlight openssh-server net-tools
 sudo aptitude -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
 sudo aptitude -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
 sudo aptitude -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev
-sudo aptitude -y autotools-dev automake libreadline-dev libelf-dev swig
+sudo aptitude -y install autotools-dev automake libreadline-dev libelf-dev swig
 sudo aptitude -y install csh lsb-core device-tree-compiler ncurses-dev
-sudo aptitude -y install unrar wine chrpath repo
+sudo aptitude -y install unrar wine chrpath repo python-pip
 
-mkdir bin
-mkdir tools
-mkdir proj
+mkdir -p ~/bin
+mkdir -p ~/tools
+mkdir -p ~/proj
 
 cp bash_env ~/
 cp emacs ~/.emacs
