298a82ef942a3a2ace6bb0bc13b373facc7474a4
Wei Song <wsong83@gmail.com>
2019-12-10 15:29:09 +0800

    using the quicker apt-fast wrapper for aptitude

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/usr/bash
 
-sudo apt-get install aptitude
+sudo apt-get update
+sudo apt-get -y install aptitude
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt-get install apt-fast
 
-sudo aptitude -y install emacs emacs-goodies-extra-el vim meld source-highlight openssh-server net-tools
-sudo aptitude -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
-sudo aptitude -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
-sudo aptitude -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev
-sudo aptitude -y install autotools-dev automake libreadline-dev libelf-dev swig
-sudo aptitude -y install csh lsb-core device-tree-compiler ncurses-dev
-sudo aptitude -y install unrar wine chrpath repo python-pip
-sudo aptitude -y install r-base-dev ess
+sudo apt-fast -y install emacs emacs-goodies-extra-el vim meld source-highlight openssh-server net-tools htop
+sudo apt-fast -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
+sudo apt-fast -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
+sudo apt-fast -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev
+sudo apt-fast -y install autotools-dev automake libreadline-dev libelf-dev swig
+sudo apt-fast -y install csh lsb-core device-tree-compiler ncurses-dev
+sudo apt-fast -y install unrar chrpath repo python-pip
+sudo apt-fast -y install r-base-dev ess
 
 mkdir -p ~/bin
 mkdir -p ~/tools
@@ -17,6 +20,7 @@ mkdir -p ~/proj
 
 cp bash_env ~/
 cp emacs ~/.emacs
+mkdir -p ~/.sbt
 cp sbt ~/.sbt/repositories
 cp -r bin/* ~/bin/
 
