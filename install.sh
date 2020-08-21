59d27cebb60b69472e09be1ef26d1d302663ee80
Wei Song (宋威) <wsong83@gmail.com>
2020-08-21 11:35:38 +0800

    add openssl headers as needed by linux kernel

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-fast -y install emacs emacs-goodies-extra-el vim meld source-highlight 
 sudo apt-fast -y install openssh-server net-tools htop nethogs sshfs nfs-client mutt sendmail
 sudo apt-fast -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
 sudo apt-fast -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
-sudo apt-fast -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev
+sudo apt-fast -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev libssl-dev
 sudo apt-fast -y install autotools-dev automake libreadline-dev libelf-dev swig gawk
 sudo apt-fast -y install csh lsb-core device-tree-compiler ncurses-dev
 sudo apt-fast -y install unrar chrpath repo python-pip
