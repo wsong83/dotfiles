f5323f671b687a807fc3cd337fcb2600d59eb3d1
Wei Song <wsong83@gmail.com>
2023-05-07 22:19:22 +0800

    replace apt-fast with aptitude as it cannot be install in china

--- install-personal.sh
+++ install-personal.sh
@@ -1,5 +1,10 @@
 #!/usr/bash
 
-sudo apt-fast -y install barrier inkscape
-sudo apt-fast -y install r-base r-cran-dplyr r-cran-ggridges
-sudo apt-fast -y install gnome-shell-extension-manager
+# for apt-fast
+sudo add-apt-repository -y ppa:apt-fast/stable
+sudo apt-get install apt-fast
+
+sudo aptitude -y install barrier inkscape
+sudo aptitude -y install r-base r-cran-dplyr r-cran-ggridges
+sudo aptitude -y install gnome-shell-extension-manager
+sudo aptitude -y install texlive-xetex latex-cjk-chinese texlive-lang-cjk
--- install.sh
+++ install.sh
@@ -3,31 +3,18 @@
 # this script is partially suited for Ubuntu-16.04 and still OK for Ubuntu-22.04
 
 sudo apt-get update
-
 sudo apt-get -y install aptitude curl
-# for apt-fast
-sudo add-apt-repository -y ppa:apt-fast/stable
-# for python 3.6+
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-# for git 2.9+
-sudo add-apt-repository -y ppa:git-core/ppa
-# for gcc-6+
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-
-sudo apt-get update
-sudo apt-get install apt-fast
-
-sudo apt-fast -y install emacs emacs-goodies-extra-el vim meld source-highlight screen xterm git plocate
-sudo apt-fast -y install openssh-server net-tools htop nethogs sshfs nfs-client mutt sendmail lvm2
-sudo apt-fast -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
-sudo apt-fast -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
-sudo apt-fast -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev libssl-dev
-sudo apt-fast -y install autotools-dev automake libreadline-dev libelf-dev swig gawk
-sudo apt-fast -y install csh lsb-core device-tree-compiler ncurses-dev
-sudo apt-fast -y install unrar chrpath repo python-pip
-sudo apt-fast -y install fail2ban
-sudo apt-fast -y install scala-mode-el
-sudo apt-fast -y upgrade
+sudo aptitude -y install emacs emacs-goodies-extra-el vim meld source-highlight screen xterm git plocate
+sudo aptitude -y install openssh-server net-tools htop nethogs sshfs nfs-client mutt sendmail lvm2
+sudo aptitude -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
+sudo aptitude -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
+sudo aptitude -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev libssl-dev
+sudo aptitude -y install autotools-dev automake libreadline-dev libelf-dev swig gawk
+sudo aptitude -y install csh lsb-core device-tree-compiler ncurses-dev
+sudo aptitude -y install unrar chrpath repo python-pip
+sudo aptitude -y install fail2ban
+sudo aptitude -y install scala-mode-el
+sudo aptitude -y upgrade
 
 mkdir -p ~/bin
 mkdir -p ~/tools
