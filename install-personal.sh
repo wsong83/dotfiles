ed4a62a66857fd1dd1398b433537e344c3f94558
Wei Song <wsong83@gmail.com>
2023-05-15 14:23:24 +0800

    add apt timeout

--- install-personal.sh
+++ install-personal.sh
@@ -10,3 +10,6 @@ sudo apt-fast -y install texlive-xetex latex-cjk-chinese texlive-lang-cjk
 sudo apt-fast -y install gnucash
 sudo apt-fast -y install r-base r-cran-dplyr r-cran-ggridges
 sudo apt-fast -y install gnome-shell-extension-manager
+
+sudo cp 99timeout /etc/apt/apt.conf.d/
+sudo chown root:root /etc/apt/apt.conf.d/99timeout
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get -y install aptitude curl
 sudo aptitude -y install emacs emacs-goodies-extra-el vim meld source-highlight screen xterm git plocate
 sudo aptitude -y install openssh-server net-tools htop nethogs sshfs nfs-client mutt sendmail lvm2
-sudo aptitude -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended
+sudo aptitude -y install texlive-extra-utils texlive-fonts-recommended texlive-latex-recommended texlive-science texlive-latex-extra texlive-fonts-extra
 sudo aptitude -y install texlive-science texlive-latex-extra gnuplot xfonts-100dpi xfonts-75dpi t1-xfree86-nonfree ttf-xfree86-nonfree
 sudo aptitude -y install libboost-all-dev texinfo flex bison libgmp-dev libmpfr-dev libmpc-dev zlib1g-dev libssl-dev
 sudo aptitude -y install autotools-dev automake libreadline-dev libelf-dev swig gawk
